a406063083b83fc3dbabb61f3cded6898af7e50d
Gaurav <gaurav.dwivedi2@5paisa.com>
2023-06-07 16:49:01 +0530

    Added TOTP login

--- const.js
+++ const.js
@@ -5,91 +5,113 @@ const followingDay = date.getTime();
 
 const GENERIC_PAYLOAD = {
   head: {
-    appName: "",
-    appVer: "1.0",
-    key: "",
-    osName: "WEB",
-    requestCode: "",
-    userId: "",
-    password: ""
+    appName: '',
+    appVer: '1.0',
+    key: '',
+    osName: 'WEB',
+    requestCode: '',
+    userId: '',
+    password: '',
   },
   body: {
-    ClientCode: ""
-  }
+    ClientCode: '',
+  },
 };
+
+const TOTP_PAYLOAD = {
+  head: {
+    Key: '',
+  },
+  body: {
+    Email_ID: '',
+    TOTP: '',
+    PIN: '',
+  },
+};
+
+const ACCESS_TOKEN_PAYLOAD = {
+  head: {
+    Key: '',
+  },
+  body: {
+    RequestToken: '',
+    EncryKey: '',
+    UserId: '',
+  },
+};
+
 const MARKET_PAYLOAD = {
   head: {
-    appName: "",
-    appVer: "1.0",
-    key: "",
-    osName: "WEB",
-    requestCode: "5PMF",
-    userId: "",
-    password: ""
+    appName: '',
+    appVer: '1.0',
+    key: '',
+    osName: 'WEB',
+    requestCode: '5PMF',
+    userId: '',
+    password: '',
   },
   body: {
-    Count: "",
+    Count: '',
     MarketFeedData: [],
-    ClientLoginType:0,
-    LastRequestTime:`/Date(${today})/`,
-    RefreshRate:"H"
-  }
+    ClientLoginType: 0,
+    LastRequestTime: `/Date(${today})/`,
+    RefreshRate: 'H',
+  },
 };
 
 const LOGIN_PAYLOAD = {
   head: {
-    appName: "",
-    appVer: "1.0",
-    key: "",
-    osName: "WEB",
-    requestCode: "5PLoginV2",
-    userId: "",
-    password: ""
+    appName: '',
+    appVer: '1.0',
+    key: '',
+    osName: 'WEB',
+    requestCode: '5PLoginV2',
+    userId: '',
+    password: '',
   },
   body: {
-    Email_id: "",
-    Password: "",
-    LocalIP: "192.168.1.1",
-    PublicIP: "192.168.1.1",
-    HDSerailNumber: "",
-    MACAddress: "",
-    MachineID: "039377",
-    VersionNo: "1.7",
-    RequestNo: "1",
-    My2PIN: "",
-    ConnectionType: "1"
-  }
+    Email_id: '',
+    Password: '',
+    LocalIP: '192.168.1.1',
+    PublicIP: '192.168.1.1',
+    HDSerailNumber: '',
+    MACAddress: '',
+    MachineID: '039377',
+    VersionNo: '1.7',
+    RequestNo: '1',
+    My2PIN: '',
+    ConnectionType: '1',
+  },
 };
-
 
 const ORDER_PLACEMENT_PAYLOAD = {
   head: {
-    appName: "",
-    appVer: "1.0",
-    key: "",
-    osName: "WEB",
-    requestCode: "",
-    userId: "",
-    password: ""
+    appName: '',
+    appVer: '1.0',
+    key: '',
+    osName: 'WEB',
+    requestCode: '',
+    userId: '',
+    password: '',
   },
   body: {
-    ClientCode: "1234567",
-    Exchange: "B",
-    ExchangeType: "C",
+    ClientCode: '1234567',
+    Exchange: 'B',
+    ExchangeType: 'C',
     Price: 0.0,
     OrderID: 0,
-    OrderType: "BUY",
+    OrderType: 'BUY',
     Qty: 0,
-    UniqueOrderID: "1",
+    UniqueOrderID: '1',
     DisQty: 0,
     IsStopLossOrder: false,
     StopLossPrice: 0,
     IsIOCOrder: false,
     IsIntraday: false,
-    IsAHOrder: "N",
+    IsAHOrder: 'N',
     ValidTillDate: `/Date(${followingDay})/`,
-    AppSource: 0
-  }
+    AppSource: 0,
+  },
 };
 
 /**
@@ -103,13 +125,15 @@ const OrderValidityEnum = {
   GTC: 2,
   IOC: 3,
   EOS: 4,
-  FOK: 6
+  FOK: 6,
 };
 
 module.exports = {
   genericPayload: GENERIC_PAYLOAD,
   loginPayload: LOGIN_PAYLOAD,
   orderPayload: ORDER_PLACEMENT_PAYLOAD,
-  marketpayload : MARKET_PAYLOAD,
-  OrderValidityEnum: OrderValidityEnum
+  marketpayload: MARKET_PAYLOAD,
+  totpPayload: TOTP_PAYLOAD,
+  accessTokenPayload: ACCESS_TOKEN_PAYLOAD,
+  OrderValidityEnum: OrderValidityEnum,
 };
--- utils.js
+++ utils.js
@@ -1,8 +1,23 @@
-const crypto = require("crypto");
+const crypto = require('crypto');
 
-const iv = new Buffer.from([83, 71, 26, 58, 54, 35, 22, 11,
-  83, 71, 26, 58, 54, 35, 22, 11]);
-
+const iv = new Buffer.from([
+  83,
+  71,
+  26,
+  58,
+  54,
+  35,
+  22,
+  11,
+  83,
+  71,
+  26,
+  58,
+  54,
+  35,
+  22,
+  11,
+]);
 
 const AES256Encrypt = (key, text) => {
   const keyLen = 256;
@@ -10,11 +25,11 @@ const AES256Encrypt = (key, text) => {
 
   try {
     const keyGen = crypto.pbkdf2Sync(
-      Buffer.from(key),
-      Uint8Array.from(iv),
-      1000,
-      keyLen + IVLen,
-      "sha1"
+        Buffer.from(key),
+        Uint8Array.from(iv),
+        1000,
+        keyLen + IVLen,
+        'sha1',
     );
 
     const aesKey = Buffer.allocUnsafe(32);
@@ -23,20 +38,17 @@ const AES256Encrypt = (key, text) => {
     // you need to limit the buffer copy to 16 for aesIV.
     keyGen.copy(aesIV, 0, 0, 16);
     keyGen.copy(aesKey, 0, 16);
-    const cipher = crypto.createCipheriv("aes-256-cbc", aesKey, aesIV);
+    const cipher = crypto.createCipheriv('aes-256-cbc', aesKey, aesIV);
 
-    let encrypted = cipher.update(text, "utf8", "base64");
-    encrypted += cipher.final("base64");
-    
+    let encrypted = cipher.update(text, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+
     return encrypted;
   } catch (error) {
-    console.log(error, "error in encrypt");
+    console.log(error, 'error in encrypt');
   }
 };
 
-
-
 module.exports = {
   AES256Encrypt: AES256Encrypt,
- 
 };
